ada846d8d8af322d28a6719927741885b5d97eec
FanomezantsoaRAKOTONDRAINY <Fanelsodzy@gmail.com>
2023-10-18 11:15:18 +0300

    Modification

--- html/footer.php
+++ html/footer.php
@@ -14,7 +14,7 @@
       Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Projet sayna By RAKOTONDRAINY Fanomezantsoa&copy; 2023</strong>
   </footer>
 </div>
 <!-- jQuery -->
